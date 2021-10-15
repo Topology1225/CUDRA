88d7e672d2ae7fce951cc560de8964408357d81f
Topology1225 <sato.haruki.844@s.kyushu-u.ac.jp>
2021-10-15 20:30:31 +0900

    update

--- cudra/__init__.py
+++ cudra/__init__.py
@@ -1,7 +1,7 @@
 from .cudra import *
 
 __copyright__    = 'Copyright (C) 2021 Haruki Sato'
-__version__      = '0.0.1c'
+__version__      = '0.0.4'
 __license__      = 'MIT License'
 __author__       = 'Haruki Sato'
 __author_email__ = 'e0804s.haruki@gmail.com'
