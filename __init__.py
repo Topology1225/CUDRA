1895f77caa214af8a01fe182fdd5276b10a1f90a
Topology1225 <sato.haruki.844@s.kyushu-u.ac.jp>
2021-10-15 19:47:43 +0900

    add init.py!

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .cudra import *
+
+__copyright__    = 'Copyright (C) 2021 Haruki Sato'
+__version__      = '0.0.1'
+__license__      = 'MIT License'
+__author__       = 'Haruki Sato'
+__author_email__ = 'e0804s.haruki@gmail.com'
+__url__          = 'https://github.com/Topology1225/CUDRA'
+
+__all__ = [cudra,]
