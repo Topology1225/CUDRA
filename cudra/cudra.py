6145851b755604d37fde4cb1261e427b244a4c98
Topology1225 <e0804s.haruki@gmail.com>
2021-12-22 23:53:27 +0900

    ver0.0.4

--- cudra/cudra.py
+++ cudra/cudra.py
@@ -1,11 +1,14 @@
+import os
 import subprocess
 import time
 
 import datetime
 import pynvml
 
+
 class cudra(object):
     def __init__(self, gpu_ids, sec):
+
         self.gpu_ids = gpu_ids
         self.sec = sec
 
@@ -17,8 +20,17 @@ class cudra(object):
 
         self.lt_process = list()
 
-    def run(self, script: str, arguments: list(), p=False):
+    def run(self, script: str, arguments: list(), p=False, log_prefix="./"):
         self.script = script
+        root = os.path.join(log_prefix, ".cudra/log")
+        assert os.access(root, os.W_OK), f"CUDRA cannot write {root}."
+
+        os.makedirs(root, exist_ok=True)
+        log_file = f'{datetime.datetime.now().strftime("%Y_%m_%d_%H:%M:%S")}_exec.txt'
+        logPath = os.path.join(root, log_file)
+        pipe = open(logPath, "a")
+        self.logPath = logPath
+        self.arguments = arguments
 
         cnt = 0
         while True:
@@ -40,9 +52,17 @@ class cudra(object):
 
                     if p:
                         cmd = ("echo",) + cmd
+                    else:
+                        print(" ".join(cmd))
 
                     process = subprocess.Popen(
-                        " ".join(cmd), shell=True, text=True, start_new_session=True
+                        " ".join(cmd),
+                        shell=True,
+                        text=True,
+                        start_new_session=True,
+                        stdout=open(os.devnull, "w"),
+                        stderr=pipe,
+                        stdin=open(os.devnull, "w"),
                     )
                     self.lt_process.append(process)
 
@@ -57,15 +77,24 @@ class cudra(object):
             if cnt >= len(arguments):
                 break
 
-            
             time.sleep(self.sec)
-        
+
         for p in self.lt_process:
             p.communicate()
-        
+
     def __del__(self):
         pynvml.nvmlShutdown()
         for p in self.lt_process:
             p.kill()
 
-            
+        error_execute = 0
+        f = open(self.logPath, mode="r")
+        for line in f.readlines():
+            if "Error" in line:
+                error_execute += 1
+
+        msg = (
+            f"[CUDRA]: ERROR {error_execute}/{len(self.arguments)}",
+            f"Log File Path: {self.logPath}",
+        )
+        print("\n".join(msg))
